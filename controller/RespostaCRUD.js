28fc5b654e56d46313f1ab81bba56a43bf920bf3
Paulomascaro <mascaropr@gmail.com>
2023-06-15 20:34:38 -0300

    commit 2

--- controller/RespostaCRUD.js
+++ controller/RespostaCRUD.js
@@ -1,18 +1,21 @@
+
 const Resposta = require('../modelo/index.js').Resposta;
 const router = require("express").Router(); 
 
-// cadastrando usuario
+// cadastrando Resposta
 router.post('/adicionar', async (req, res) => {
     try {
+        console.log(req.body)
         await Resposta.sync();
-        if(req.body.msg =! null) {
+        if(req.body.msg != null) {
             console.log(`inserindo Resposta no banco`);
-            const usuario = await Resposta.create({
+            const resposta = await Resposta.create({
                 msg: req.body.msg,
-                id_mensagemPrincipal: req.body.id_mensagemPrincipal
+                id_mensagemPrincipal: req.body.id_mensagemPrincipal,
+                id_usuario: req.body.id_usuario
             })
-            console.log(req.body)
-            res.json(usuario)
+            // console.log(req.body)
+            res.json(resposta)
         } else {
             res.status(422).json({Erro: "Parametros faltando"})
         }
@@ -21,34 +24,50 @@ router.post('/adicionar', async (req, res) => {
     }
 });
 
+//listando todas as respostas
+router.get('/listartodas/:id', async (req, res) => {
+    try {
+        await Resposta.sync();
+        console.log(`listando todas as respostas no banco`);
+        const resposta = await Resposta.findAll({
+           where: {id_mensagemPrincipal: req.params.id}
 
-// deletando msg
+        });
+        console.log(resposta);
+        res.send(JSON.stringify(resposta));
+    } catch (e) {
+        console.log(e)
+        res.status(500).json({Erro:"Erro no servidor!"})
+    }
+});
+
+
+// deletando resposta
 router.delete('/excluir/:id', async (req, res) => {
     try {
-        await Mensagem.sync();
-        const msgs = await Mensagem.destroy({
+        await Resposta.sync();
+        const resposta = await Resposta.destroy({
             where: {id: req.params.id}
         });
 
-        res.send(`mensagem ${req.params.id} excluido!`);
+        res.send(`Resposta ${req.params.id} excluido!`);
     } catch (e) {
         res.status(500).json({Erro:"Erro no servidor!"})
     }
 });
 
-// editar mensagem
+// editar resposta
 router.put('/editar/:id', async (req, res) => {
     try {
-        if (req.body.id != null && req.body.msg != null) {
-            await Mensagem.sync();
-
-            const msgs = await Mensagem.update({
+        if (req.body.msg != null) {
+            await Resposta.sync();
+            const resposta = await Resposta.update({
                 msg: req.body.msg,
             }, {
                 where: {id: req.params.id}
-            });
+            });resposta
 
-            res.send(`mensagem ${req.params.id} editado!`);
+            res.send(`Resposta ${req.params.id} editado!`);
         } else {
             res.status(422).json({Erro: "Parametros faltando!"})
         }
